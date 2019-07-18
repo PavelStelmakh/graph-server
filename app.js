b3d85262ced6b3a16fe886e091caa9a64fe81826
Pavel Stelmakh <pavelstelmax@rambler.ru>
2019-07-18 23:11:08 +0300

    connected graphql

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Graphql from 'graphql';
+import graphqlHTTP from 'express-graphql';
 
 import { PORT, NODE_ENV } from './config.js';
 import { userScheme, articleScheme } from './schemes/index.js';
 
 const app = express();
+
+const { buildSchema } = Graphql;
   
-const User = mongoose.model("User", userScheme);
-const Article = mongoose.model("Article", articleScheme); // при выборке  .populate('createdBy')
+const User = mongoose.model('User', userScheme);
+const Article = mongoose.model('Article', articleScheme); // при выборке  .populate('createdBy')
 
 const db = {
     User,
     Article,
 };
 
-app.use(express.json());
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
 
-mongoose.connect("mongodb://localhost:27017/db", { useNewUrlParser: true })
+const rootValue = {
+    hello: () => 'hello world',
+};
+
+// graphql(scheme, '{ hello }', root).then(response => console.log(response.data));
+
+// app.use(express.json());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+}));
+
+mongoose.connect('mongodb://localhost:27017/db_graphql', { useNewUrlParser: true })
     .then(() => {
         app.locals.db = db;
+        // app.locals.db = {
+        //     User: mongoose.model("User", userScheme),
+        //     Article: mongoose.model("Article", articleScheme),
+        // };
         return app.listen(PORT);
     })
     .then(() => console.log(`server listening port ${PORT} with ${NODE_ENV} settings`))
@@ -27,13 +53,7 @@ mongoose.connect("mongodb://localhost:27017/db", { useNewUrlParser: true })
         console.error(err);
     });
 
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
     mongoose.disconnect();
     process.exit();
 });
-// app.listen(PORT, (err) => {
-//     if (err) {
-//         throw new Error(err);
-//     }
-//     console.log(`server listening port ${PORT} with ${NODE_ENV} settings`);    
-// })
